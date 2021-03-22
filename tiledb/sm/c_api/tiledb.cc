3ee4b6671ae8832161d6272ab64dfff3cde045bd
Rebekah <bekadavis9@gmail.com>
2021-03-22 12:34:32 -0400

    A few changes to open and reopen

--- tiledb/sm/c_api/tiledb.cc
+++ tiledb/sm/c_api/tiledb.cc
@@ -3375,7 +3375,6 @@ int32_t tiledb_array_open(
   return TILEDB_OK;
 }
 
-/* This is a deprecated API */
 int32_t tiledb_array_open_at(
     tiledb_ctx_t* ctx,
     tiledb_array_t* array,
@@ -3421,7 +3420,6 @@ int32_t tiledb_array_open_with_key(
   return TILEDB_OK;
 }
 
-/* This is a deprecated API. */
 int32_t tiledb_array_open_at_with_key(
     tiledb_ctx_t* ctx,
     tiledb_array_t* array,
